c30a24b3a32388d98d61cce5e789011278c58655
bonampa312 <bonampa312@gmail.com>
2016-04-02 11:57:00 -0500

    Management Beans missing

--- src/java/co/edu/udea/modelo/Estudiante.java
+++ src/java/co/edu/udea/modelo/Estudiante.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.udea.modelo;
 
 import java.io.Serializable;
@@ -25,7 +20,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author frank.castrillon
+ * @author Camila's Team
  */
 @Entity
 @Table(name = "Estudiante")
