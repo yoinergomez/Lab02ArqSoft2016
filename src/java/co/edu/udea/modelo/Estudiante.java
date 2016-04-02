f06695fb4cfd83f03972c240c67de7cd9e942862
bonampa312 <bonampa312@gmail.com>
2016-04-02 09:45:35 -0500

    DTO's

---
+++ src/java/co/edu/udea/modelo/Estudiante.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.modelo;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author frank.castrillon
+ */
+@Entity
+@Table(name = "Estudiante")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Estudiante.findAll", query = "SELECT e FROM Estudiante e"),
+    @NamedQuery(name = "Estudiante.findByDocumento", query = "SELECT e FROM Estudiante e WHERE e.documento = :documento"),
+    @NamedQuery(name = "Estudiante.findByNombre", query = "SELECT e FROM Estudiante e WHERE e.nombre = :nombre"),
+    @NamedQuery(name = "Estudiante.findByApellido", query = "SELECT e FROM Estudiante e WHERE e.apellido = :apellido")})
+public class Estudiante implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "documento")
+    private String documento;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "apellido")
+    private String apellido;
+    @JoinTable(name = "Estudiante_Materia", joinColumns = {
+        @JoinColumn(name = "estudiante", referencedColumnName = "documento")}, inverseJoinColumns = {
+        @JoinColumn(name = "materia", referencedColumnName = "codigo")})
+    @ManyToMany
+    private Collection<Materia> materiaCollection;
+    @JoinColumn(name = "carrera", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Carrera carrera;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(String documento) {
+        this.documento = documento;
+    }
+
+    public Estudiante(String documento, String nombre, String apellido) {
+        this.documento = documento;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @XmlTransient
+    public Collection<Materia> getMateriaCollection() {
+        return materiaCollection;
+    }
+
+    public void setMateriaCollection(Collection<Materia> materiaCollection) {
+        this.materiaCollection = materiaCollection;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (documento != null ? documento.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estudiante)) {
+            return false;
+        }
+        Estudiante other = (Estudiante) object;
+        if ((this.documento == null && other.documento != null) || (this.documento != null && !this.documento.equals(other.documento))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "co.edu.udea.modelo.Estudiante[ documento=" + documento + " ]";
+    }
+    
+}
