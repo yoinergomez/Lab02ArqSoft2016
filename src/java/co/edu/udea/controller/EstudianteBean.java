67c2740d67f6c26ee929ab11ad196c8df1951170
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-04-07 19:03:16 -0500

    Agregando funcionalidades los botones de la vista Estudiante.xhtml

--- src/java/co/edu/udea/controller/EstudianteBean.java
+++ src/java/co/edu/udea/controller/EstudianteBean.java
@@ -3,21 +3,24 @@ package co.edu.udea.controller;
 import co.edu.udea.logica.EstudianteFacadeLocal;
 import java.io.Serializable;
 import co.edu.udea.modelo.Estudiante;
+import co.edu.udea.modelo.Materia;
 import java.awt.Event;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-
 public class EstudianteBean implements Serializable {
 
     @EJB
     private EstudianteFacadeLocal estudianteDAO;
     private List<Estudiante> studentList;
-    
     private String cedula;
+    private String desabilitarVerMatricula;
+    private String desabilitarMatricula;
 
     @PostConstruct
     public void init() {
@@ -25,13 +28,32 @@ public class EstudianteBean implements Serializable {
     }
 
     public EstudianteBean() {
-
+        desabilitarVerMatricula = "none";
+        desabilitarMatricula = "none";
     }
 
     public void matricularEstudiante() {
         System.out.println(cedula);
+        Estudiante estudiante = estudianteDAO.find(cedula);
+        if (estudiante != null) {
+            studentList.clear();
+            studentList.add(estudiante);
+            Collection<Materia> materiaEstudiante = estudiante.getMateriaCollection();
+            
+            // El estudiante esta matriculado?
+            if(materiaEstudiante.isEmpty()){
+               desabilitarMatricula = "block";
+               desabilitarVerMatricula = "none";
+            } else {
+               desabilitarMatricula = "none";
+               desabilitarVerMatricula = "block";
+            }
+        } else{
+            desabilitarMatricula = "none";
+            desabilitarVerMatricula = "none";
+        }
     }
-    
+
     public List<Estudiante> getStudentList() {
         return studentList;
     }
@@ -52,4 +74,20 @@ public class EstudianteBean implements Serializable {
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
+
+    public String getDesabilitarVerMatricula() {
+        return desabilitarVerMatricula;
+    }
+
+    public void setDesabilitarVerMatricula(String desabilitarVerMatricula) {
+        this.desabilitarVerMatricula = desabilitarVerMatricula;
+    }
+
+    public String getDesabilitarMatricula() {
+        return desabilitarMatricula;
+    }
+
+    public void setDesabilitarMatricula(String desabilitarMatricula) {
+        this.desabilitarMatricula = desabilitarMatricula;
+    } 
 }
